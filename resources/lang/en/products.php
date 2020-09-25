a3a8bbc91ff6e83d02977bc3d2ef53cfb8977ae0
Alexander.Shcherbakov <Alexander.Shcherbakov@cadfem-cis.ru>
2020-09-25 14:04:22 +0300

    add resources

---
+++ resources/lang/en/products.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'productJsonOptions' => [
+        'inserted' => 'Product inserted in DB',
+        'exist' => 'Product existed in DB',
+        'error' => 'Something is wrong'
+    ]
+];
